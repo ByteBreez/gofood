15129ad7a9ef1bcf7c544b4aac36369f3a27805e
ByteBreez <jai123shriganesh@gmail.com>
2024-11-30 13:17:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const passwordBox = document.getElementById("password");
+
+function createPassword() {
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHI{}[]+=JKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+  // length of password = 12
+  let password = "";
+
+  for (let i = 0; i < 12; i++) {
+    const ch = Math.floor(Math.random() * chars.length);
+    password += chars[ch];
+  }
+
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
